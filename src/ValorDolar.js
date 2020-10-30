3d8bba3110d400f0ecb41040fc365a8aac7bf29e
Anastasiia Fedorova <miffyrli@gmail.com>
2020-10-30 11:05:14 -0300

    serie + return fix

--- src/ValorDolar.js
+++ src/ValorDolar.js
@@ -1,15 +1,23 @@
 import React, {useState, useEffect} from 'react';
 import axios from "axios";
+import {ListGroup} from "react-bootstrap";
 
 function ValorDolar(props) {
 	const [valores, setValores] = useState([]);
 
 	useEffect(() => {
-		axios.get("https://mindicador.cl/api/dolar")
-			.then(res => {
-				const data = res.data;
-				setValores(data.series);
-			})
+		axios
+		.get("https://mindicador.cl/api/dolar")
+		.then(res => {
+		const data = res.data;
+		setValores(data.serie);
+		})
 	}, []);
-
+	return(
+		<ListGroup>
+			{valores.map((v) => <ListGroup.Item>{v.valor} {v.fecha}</ListGroup.Item>)}
+		</ListGroup>
+	);
 }
+
+export default ValorDolar;
