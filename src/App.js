327f8b522309b84b4501eff05f2b69897a2e43a3
VadokDev <gonzalo.fdez06@gmail.com>
2020-11-07 01:25:29 -0300

    react router

--- src/App.js
+++ src/App.js
@@ -8,90 +8,98 @@ import { Navbar, Nav, Form, FormControl, Button, Container, Row, Col, Card } fro
 import ValorDolar from "./ValorDolar.js";
 import Counter from "./Counter.js";
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+
 function App() {
   return (
-    <div className="App">
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Nav> 
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-info">Search</Button>
-        </Form>
-      </Navbar>
+    <Router>
       
-      <Container fluid>
-        <Row>
-          <Col>
-            <Counter />
-          </Col>
-          <Col>
-            <Counter />
-          </Col>
-          <Col>
-            <Counter />
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <ValorDolar />
-          </Col>
-          <Col>
-            <ValorDolar />
-          </Col>
-          <Col>
-            <ValorDolar />
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Tarea
-              encargado="VadokDev"
-              descripcion="Terminar de grabar el taller"
-            />
-          </Col>
-          <Col>
-            <Tarea
-              encargado="VadokDev"
-              descripcion="Terminar de grabar el taller"
-            />
-          </Col>
-          <Col>
-            <Tarea
-              encargado="VadokDev"
-              descripcion="Terminar de grabar el taller"
-            />
-          </Col>
-          <Col>
-            <Tarea
-              encargado="VadokDev"
-              descripcion="Terminar de grabar el taller"
-            />
-          </Col>
-        </Row>
-      </Container>
-
-      <Tarea
-        encargado="VadokASDAFSDev"
-        descripcion="Terminar de ASDgrabar el taller"
-      />
-      <Tarea
-        encargado="VadoSDFSDFkDev"
-        descripcion="Terminar de gASDrabar el taller"
-      />
-      <Tarea
-        encargado="VadokASDASDDev"
-        descripcion="Terminar de grabar ASDel taller"
-      />
-      <Tarea
-        encargado="VadokDASDASDDASASDASDev"
-        descripcion="Terminar de grabar eASDASDl taller"
-      />
-    </div>
+      <div className="App">
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link as={Link} to="/contadores" href="#home">Contadores</Nav.Link>
+            <Nav.Link as={Link} to="/dolar" href="#features">Dólar</Nav.Link>
+            <Nav.Link as={Link} to="/tareas" href="#pricing">Tareas</Nav.Link>
+          </Nav> 
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-info">Search</Button>
+          </Form>
+        </Navbar>
+        
+        <Switch>
+          <Route path="/contadores">
+            <Container fluid>
+              <Row>
+                <Col>
+                  <Counter />
+                </Col>
+                <Col>
+                  <Counter />
+                </Col>
+                <Col>
+                  <Counter />
+                </Col>
+              </Row>
+            </Container>
+          </Route>
+          <Route path="/dolar">
+            <Container fluid>
+              <Row>
+                <Col>
+                  <ValorDolar />
+                </Col>
+                <Col>
+                  <ValorDolar />
+                </Col>
+                <Col>
+                  <ValorDolar />
+                </Col>
+              </Row>
+            </Container>
+          </Route>
+          <Route path="/tareas">
+            <Container fluid>
+              <Row>
+                <Col>
+                  <Tarea
+                    encargado="VadokDev"
+                    descripcion="Terminar de grabar el taller"
+                  />
+                </Col>
+                <Col>
+                  <Tarea
+                    encargado="VadokDev"
+                    descripcion="Terminar de grabar el taller"
+                  />
+                </Col>
+                <Col>
+                  <Tarea
+                    encargado="VadokDev"
+                    descripcion="Terminar de grabar el taller"
+                  />
+                </Col>
+                <Col>
+                  <Tarea
+                    encargado="VadokDev"
+                    descripcion="Terminar de grabar el taller"
+                  />
+                </Col>
+              </Row>
+            </Container>
+          </Route>
+          <Route path="/">
+            <div>Ola</div>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
